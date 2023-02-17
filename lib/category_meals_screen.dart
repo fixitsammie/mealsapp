f91d9047068e2906131d4827ec51a45660a21354
Samuel Adewole <samadewole381@gmail.com>
2023-02-17 23:29:20 +0100

    set title as app bar title on meal screen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('The Recipes')),
+      appBar: AppBar(title: Text(categoryTitle)),
       body: const Center(
         child: Text(
           'The Recipes for the category',
