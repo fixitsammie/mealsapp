d5bdb78da9e3e5eb90c182217c5b1e064af80a18
Samuel Adewole <samadewole381@gmail.com>
2023-03-01 20:55:52 +0100

    added duration, affordability and complexity to meal item

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import 'package:mealsapp/widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   //final String categoryId;
@@ -22,7 +23,13 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(title: Text(args['title'])),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+          );
         },
         itemCount: categoryMeals.length,
       ),
